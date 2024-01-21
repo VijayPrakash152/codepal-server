9c3244e2a1ad60962670066ccad56e3a15b04d7c
Vijay Prakash <singhsidd152@gmail.com>
2024-01-21 21:46:03 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "welocome to codepal server testing!";
+    return "welocome to codepal server!";
   }
 }
